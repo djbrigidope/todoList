47fc193a5453af09b1e56933c9bd308806b18000
B.O.A.T <PRIME29@MODEL-PRIME.local>
2017-04-12 17:00:58 -0700

    adds jade modules and fixed file directory to project...

---
+++ expresstodo/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+
+// Set the views directory and template engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+// Set our static dire222ctory for public assets like client scripts
+app.use(express.static('public'));
+
+// Parses the body on incoming requests
+app.use(bodyParser.json());
+
+// Pretty prints HTML output
+app.locals.pretty = true;
+
+// Define our main route, HTTP "GET /" which will print "hello"
+app.get('/', function (req, res) {
+   res.render('index');
+});
+
+//tag route handler
+app.get('/tags/:name.tag', function(req, res) {
+  var name = 'tag-' + req.params.name;
+  res.render('../client/' + name);
+})
+
+// Start listening for connections
+app.listen(3000, function (err) {
+    if (err) {
+        console.error('Cannot listen at port 3000', err);
+    }
+    console.log('Todo app listening at port 3000');
+});
